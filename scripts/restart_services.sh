71364458a75d09b8514d05b42f108857e0de4efc
Masaki <masaki0320@icloud.com>
2019-03-16 23:29:13 +0200

    sh

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -8,11 +8,11 @@
     source ../envvars.txt
 } 
 
-
 docker rm -f mongo
 docker rm -f playground
 docker rm -f rest
 
-./root/origami-blockchain/scripts/helpers/startMongoDB.sh
-./root/origami-blockchain/scripts/helpers/startRestServer.sh
-./root/origami-blockchain/scripts/helpers/startPlayground.sh
+cd /root/origami-blockchain/scripts/helpers
+./startMongoDB.sh
+./startRestServer.sh
+./startPlayground.sh
